7d519425c3ebee3733f567ecfd335ef6116eebc4
YCHE0392 <stanpoldark@gmail.com>
2024-06-03 18:40:52 +1000

    Revert "update"
    This reverts commit 09b001bf1a490e6be32ae76c29f874a2910e77ee.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import router from './router'
 
 Amplify.configure(outputs);
 
-createApp(App).use(router).mount("#app");
+createApp(App).mount("#app");
